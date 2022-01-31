079e7a24fa48c1afec813ecfa4a36f626fbd517b
yasaraltunbas <yasar.altunbas34@gmail.com>
2022-01-31 16:49:24 +0300

    Order

--- order-subdomain/src/main/java/order/domain/Order.java
+++ order-subdomain/src/main/java/order/domain/Order.java
@@ -1,10 +1,70 @@
 package order.domain;
 
+import customer.domain.Customer;
+
+import book.domain.Book;
+
+import java.util.Objects;
 
 public class Order {
-  //  private final OrderId orderId;
-  //  private Customer customer;
-   // private Book book;
+    private OrderId orderId;
+   private Book book;
+   private Customer customer;
 
+    public Order(OrderId orderId, Book book, Customer customer) {
+        this.orderId = orderId;
+        this.book = book;
+        this.customer = customer;
+    }
 
+    public OrderId getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(OrderId orderId) {
+        this.orderId = orderId;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(orderId, order.orderId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", book=" + book +
+                ", customer=" + customer +
+                '}';
+    }
+    public static class Builder {
+        private OrderId orderId;
+        private Book book;
+        private Customer customer;
+    }
 }
