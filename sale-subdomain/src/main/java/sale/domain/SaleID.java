f2547ca84e6b02b2c093e2255efcb0115cbc2e36
kaanyldz98 <kaanyldz98@hotmail.com>
2022-02-01 12:36:14 +0300

    kaan

--- sale-subdomain/src/main/java/sale/domain/SaleID.java
+++ sale-subdomain/src/main/java/sale/domain/SaleID.java
@@ -1,2 +1,29 @@
-package sale.domain;public class SaleID {
+package sale.domain;
+
+import java.util.Objects;
+
+public class SaleID {
+    private final String saleID;
+
+    public SaleID(String saleID) {
+        this.saleID = saleID;
+    }
+
+    public String getSaleID() {
+        return saleID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SaleID saleID1 = (SaleID) o;
+        return saleID.equals(saleID1.saleID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(saleID);
+    }
+
 }
--- sale-subdomain/src/main/java/sale/domain/TimeStamp.java
+++ sale-subdomain/src/main/java/sale/domain/TimeStamp.java
@@ -1,2 +1,36 @@
-package sale.domain;public class TimeStamp {
+package sale.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class TimeStamp {
+    private final LocalDateTime timeStamp;
+
+    public TimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TimeStamp timeStamp1 = (TimeStamp) o;
+        return timeStamp.equals(timeStamp1.timeStamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timeStamp);
+    }
+
+    @Override
+    public String toString() {
+        return "TimeStamp{" +
+                "timeStamp=" + timeStamp +
+                '}';
+    }
 }
