2a6dda0f849716471e0aadaba6e3cbc9d04779c1
halilkose1 <halilkose001@gmail.com>
2022-02-01 18:25:38 +0300

    customer paketine app., business, events, repository ... eklendi

---
+++ customer-domain/src/main/java/customer/application/business/StandardCustomerApplication.java
@@ -0,0 +1,28 @@
+package customer.application.business;
+
+import customer.application.CustomerApplication;
+import customer.domain.Customer;
+import customer.domain.Identity;
+import customer.repository.CustomerRepository;
+
+import java.util.Optional;
+
+public class StandardCustomerApplication implements CustomerApplication {
+    private CustomerRepository customerRepository;
+    
+
+    @Override
+    public Customer addCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer removeCustomer(Identity identity) {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> findCustomerByIdentity(Identity identity) {
+        return Optional.empty();
+    }
+}
