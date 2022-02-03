d7d2dea4d9146cfddb8c45b38224946eb1156a4a
halilkose1 <halilkose001@gmail.com>
2022-02-03 15:16:08 +0300

    stoğa isbn validation eklendi

--- bookstore/src/main/java/com/example/bookstore/entity/BookStock.java
+++ bookstore/src/main/java/com/example/bookstore/entity/BookStock.java
@@ -1,6 +1,7 @@
 package com.example.bookstore.entity;
 
 
+import com.example.bookstore.validation.IsbnValidation;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import java.util.Objects;
 @DynamicUpdate
 public class BookStock {
     @Id
+    @IsbnValidation
     private String isbn;
     private int copiesLeft;
     private int copiesSold;
