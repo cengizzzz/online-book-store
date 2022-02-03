d1c7cb7f3357ff3c0d188b8eb7ed621e0a195fc5
halilkose1 <halilkose001@gmail.com>
2022-02-03 14:12:53 +0300

    customer+category

---
+++ bookstore/src/main/java/com/example/bookstore/entity/Category.java
@@ -0,0 +1,62 @@
+package com.example.bookstore.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="categories")
+public class Category {
+    @Id
+    private int id;
+    private String name;
+    private String subCategory;
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(String subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", subCategory='" + subCategory + '\'' +
+                '}';
+    }
+}
