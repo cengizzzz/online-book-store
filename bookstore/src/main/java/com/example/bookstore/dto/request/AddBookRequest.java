4eb7ff64dc9e6015f31ad3181b65a1afec79469e
halilkose1 <halilkose001@gmail.com>
2022-02-04 12:57:33 +0300

    request,response,service

--- bookstore/src/main/java/com/example/bookstore/dto/request/AddBookRequest.java
+++ bookstore/src/main/java/com/example/bookstore/dto/request/AddBookRequest.java
@@ -4,11 +4,8 @@ import category.domain.Category;
 import com.example.bookstore.validation.IsbnValidation;
 import publisher.domain.Publisher;
 
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
+import java.util.Arrays;
 import java.util.List;
 
 public class AddBookRequest {
@@ -28,29 +25,121 @@ public class AddBookRequest {
     private byte[] coverPhoto;
     private List<String> reviews;
     private Category category;
-/*
-    @Id
-    @IsbnValidation
-    private String isbn;
-    @NotBlank
-    private String title;
-    @NotBlank
-    private String author;
-    @ManyToOne
-    private com.example.bookstore.entity.Publisher publisher;
-    @NotBlank
-    private String edition;
-    private int publicationYear;
-    private double price;
-    @ElementCollection
-    private List<String> content;
-    private int popularity;
-    @Column(columnDefinition = "longblob")
-    private Byte[] coverPhoto;
-    @ElementCollection
-    private List<String> reviews;
-    @ManyToOne
-    private com.example.bookstore.entity.Category category;
 
- */
+    public AddBookRequest() {
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<String> getContent() {
+        return content;
+    }
+
+    public void setContent(List<String> content) {
+        this.content = content;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
+    public byte[] getCoverPhoto() {
+        return coverPhoto;
+    }
+
+    public void setCoverPhoto(byte[] coverPhoto) {
+        this.coverPhoto = coverPhoto;
+    }
+
+    public List<String> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<String> reviews) {
+        this.reviews = reviews;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "AddBookRequest{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publisher=" + publisher +
+                ", edition='" + edition + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", price=" + price +
+                ", content=" + content +
+                ", popularity=" + popularity +
+                ", coverPhoto=" + Arrays.toString(coverPhoto) +
+                ", reviews=" + reviews +
+                ", category=" + category +
+                '}';
+    }
 }
