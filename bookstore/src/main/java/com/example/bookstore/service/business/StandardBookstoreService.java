1010c23cfd43f67fe4127fcca7a7e5e89228b885
halilkose1 <halilkose001@gmail.com>
2022-02-04 15:08:04 +0300

    +

--- bookstore/src/main/java/com/example/bookstore/service/business/StandardBookstoreService.java
+++ bookstore/src/main/java/com/example/bookstore/service/business/StandardBookstoreService.java
@@ -1,5 +1,7 @@
 package com.example.bookstore.service.business;
 
+import book.application.BookApplication;
+import book.application.business.exception.BookNotFoundException;
 import com.example.bookstore.dto.request.AddBookRequest;
 import com.example.bookstore.dto.request.UpdateRequest;
 import com.example.bookstore.dto.response.AddBookResponse;
@@ -7,14 +9,26 @@ import com.example.bookstore.dto.response.DeleteBookResponse;
 import com.example.bookstore.dto.response.GetBookResponse;
 import com.example.bookstore.dto.response.UpdateBookResponse;
 import com.example.bookstore.service.BookstoreService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StandardBookstoreService implements BookstoreService {
 
+    private BookApplication bookApplication;
+    private ModelMapper modelMapper;
+
+    public StandardBookstoreService(BookApplication bookApplication, ModelMapper modelMapper) {
+        this.bookApplication = bookApplication;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
-    public GetBookResponse findBookByIdentity(String isbn) {
-        return null;
+    public GetBookResponse findBookByIsbn(String isbn) {
+        var book = bookApplication.findBookByIsbn(isbn.valueOf(isbn));
+        if (book.isEmpty())
+            throw new BookNotFoundException("Cannot find book", isbn);
+        return modelMapper.map(book.get(), GetBookResponse.class);
     }
 
     @Override
@@ -28,7 +42,7 @@ public class StandardBookstoreService implements BookstoreService {
     }
 
     @Override
-    public DeleteBookResponse deleteBookByIdentity(String isbn) {
+    public DeleteBookResponse deleteBookByIsbn(String isbn) {
         return null;
     }
 }
