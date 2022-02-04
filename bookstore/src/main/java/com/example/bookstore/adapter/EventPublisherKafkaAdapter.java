deafe2f85d91438118d38478c309dcb3a3b186d5
cengizzzz <c.atila60@gmail.com>
2022-02-04 14:42:06 +0300

    plus

--- bookstore/src/main/java/com/example/bookstore/adapter/EventPublisherKafkaAdapter.java
+++ bookstore/src/main/java/com/example/bookstore/adapter/EventPublisherKafkaAdapter.java
@@ -15,9 +15,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import requistion.application.business.events.RequistionEvent;
+import sale.application.business.events.SaleEvent;
 
 public class EventPublisherKafkaAdapter implements category.infrastructure.EventPublisher,
-        EventPublisher, CustomerEventPublisher, StockEventPublisher, OrderEventPublisher {
+        EventPublisher, CustomerEventPublisher, StockEventPublisher, OrderEventPublisher
+        ,requistion.infrastructure.EventPublisher,
+        publisher.infrastructure.PublisherEventPublisher,sale.infrastructure.SaleEventPublisher{
 
     private static final Logger logger =
             LoggerFactory.getLogger(EventPublisherKafkaAdapter.class);
@@ -31,9 +35,12 @@ public class EventPublisherKafkaAdapter implements category.infrastructure.Event
     private String stockTopicName;
     @Value("${order.events.topic}")
     private String orderTopicName;
-    //private String publisherTopicName;
-    //private String requistion;
-    //private String saleTopicName;
+    @Value("${publisher.events.topic}")
+    private String publisherTopicName;
+    @Value("${requistion.events.topic}")
+    private String orderTopicRequistion;
+    @Value("${sale.events.topic}")
+    private String saleTopicName;
 
     private KafkaTemplate<String, String> kafkaTemplate;
     private ObjectMapper mapper;
@@ -92,6 +99,28 @@ public class EventPublisherKafkaAdapter implements category.infrastructure.Event
         try {
             var eventAsJson = mapper.writeValueAsString(businessEvent);
             kafkaTemplate.send(orderTopicName, eventAsJson);
+        } catch (JsonProcessingException e) {
+            logger.error("Error while converting the event to json: {}",
+                    e.getMessage());
+        }
+    }
+
+    @Override
+    public void publishEvent(RequistionEvent businessEvent) {
+        try {
+            var eventAsJson = mapper.writeValueAsString(businessEvent);
+            kafkaTemplate.send(orderTopicRequistion, eventAsJson);
+        } catch (JsonProcessingException e) {
+            logger.error("Error while converting the event to json: {}",
+                    e.getMessage());
+        }
+    }
+
+    @Override
+    public void publishEvent(SaleEvent businessEvent) {
+        try {
+            var eventAsJson = mapper.writeValueAsString(businessEvent);
+            kafkaTemplate.send(saleTopicName, eventAsJson);
         } catch (JsonProcessingException e) {
             logger.error("Error while converting the event to json: {}",
                     e.getMessage());
